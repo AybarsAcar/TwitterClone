c07f112674af971a5fabaf4ce601a16aedc1f946
Aybars Acar <aybars.dev@gmail.com>
2022-03-31 18:33:53 +1100

    Project Structure created

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Aybars Acar on 31/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Aybars Acar on 31/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
